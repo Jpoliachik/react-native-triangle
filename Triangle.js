147b1f610d43ac65897a9748168539a33e0e64d3
Justin Poliachik <jpoliachik@gmail.com>
2015-10-31 11:27:15 -0400

    Added support for up-left, up-right, down-left, down-right.

--- Triangle.js
+++ Triangle.js
@@ -8,6 +8,13 @@
 
  var Triangle = React.createClass({
 
+   propTypes: {
+     direction: React.PropTypes.oneOf(['up', 'right', 'down', 'left', 'up-right', 'up-left', 'down-right', 'down-left']),
+     width: React.PropTypes.number,
+     height: React.PropTypes.number,
+     color: React.PropTypes.string,
+   },
+
    getDefaultProps: function() {
      return {
        direction: 'up',
@@ -62,8 +69,52 @@
          borderBottomColor: 'transparent',
          borderLeftColor: 'transparent',
        };
+     } else if (this.props.direction == 'up-left') {
+       return {
+         borderTopWidth: this.props.height,
+         borderRightWidth: this.props.width,
+         borderBottomWidth: 0,
+         borderLeftWidth: 0,
+         borderTopColor: this.props.color,
+         borderRightColor: 'transparent',
+         borderBottomColor: 'transparent',
+         borderLeftColor: 'transparent',
+       };
+     } else if (this.props.direction == 'up-right') {
+       return {
+         borderTopWidth: 0,
+         borderRightWidth: this.props.width,
+         borderBottomWidth: this.props.height,
+         borderLeftWidth: 0,
+         borderTopColor: 'transparent',
+         borderRightColor: this.props.color,
+         borderBottomColor: 'transparent',
+         borderLeftColor: 'transparent',
+       };
+     } else if (this.props.direction == 'down-left') {
+       return {
+         borderTopWidth: this.props.height,
+         borderRightWidth: 0,
+         borderBottomWidth: 0,
+         borderLeftWidth: this.props.width,
+         borderTopColor: 'transparent',
+         borderRightColor: 'transparent',
+         borderBottomColor: 'transparent',
+         borderLeftColor: this.props.color,
+       };
+     } else if (this.props.direction == 'down-right') {
+       return {
+         borderTopWidth: 0,
+         borderRightWidth: 0,
+         borderBottomWidth: this.props.height,
+         borderLeftWidth: this.props.width,
+         borderTopColor: 'transparent',
+         borderRightColor: 'transparent',
+         borderBottomColor: this.props.color,
+         borderLeftColor: 'transparent',
+       };
      } else {
-       console.log('Error: Triangle.js wrong direction.');
+       console.error('Triangle.js wrong direction. ' + this.props.direction + ' is invalid. Must be one of: ' + ['up', 'right', 'down', 'left', 'up-right', 'up-left', 'down-right', 'down-left']);
        return {};
      }
    },
